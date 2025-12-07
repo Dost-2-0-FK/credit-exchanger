ee39ef92452a181f801964458e88dc51631078d7
Sophia Kunze <sophia.kunze@gmail.com>
2025-12-07 17:20:22 +0100

    feat: first routes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{App, HttpServer, middleware::Logger, web};
 
-use crate::service::{echo, hello};
+use crate::service::{echo, hello, post_credit_booking, post_resource_booking};
 
 mod service;
 
@@ -11,9 +11,13 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let logger = Logger::default();
 
-        App::new()
-            .wrap(logger)
-            .service(web::scope("/api").service(hello).service(echo))
+        App::new().wrap(logger).service(
+            web::scope("/api")
+                .service(hello)
+                .service(echo)
+                .service(post_credit_booking)
+                .service(post_resource_booking),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/service.rs
+++ src/service.rs
@@ -1,4 +1,5 @@
-use actix_web::{HttpResponse, Responder, get, post};
+use actix_web::{HttpResponse, Responder, get, post, web};
+use serde::Deserialize;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -8,4 +9,43 @@ async fn hello() -> impl Responder {
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
+}
+
+#[derive(Deserialize, Debug)]
+struct CreditBooking {
+    id: String,
+    receiver: String,
+    // TODO check whether value is always positive and not a float
+    value: u32,
+}
+
+#[post("/credits/book")]
+/// `POST /api/credits/book?id=<unique_id>&receiver=<receiver_id>&value=<value>`
+async fn post_credit_booking(credit_booking: web::Query<CreditBooking>) -> impl Responder {
+    let _credit_booking = credit_booking.into_inner();
+    HttpResponse::Ok()
+}
+
+#[derive(Deserialize, Debug)]
+enum ResourceBooking {
+    WithResource {
+        id: String,
+        receiver: String,
+        resource: String,
+        // TODO check whether value is always positive and not a float
+        value: u32,
+    },
+    WithoutResource {
+        id: String,
+        receiver: String,
+        // TODO should vec len >= 1?
+        value: Vec<u32>,
+    },
+}
+
+#[post("/resource/book")]
+/// `POST /api/resource/book?id=<unique_id>&receiver=<receiver_id>&resource=<resource>&value=<value>`
+async fn post_resource_booking(resource_booking: web::Query<ResourceBooking>) -> impl Responder {
+    let _resource_booking = resource_booking.into_inner();
+    HttpResponse::Ok()
 }
